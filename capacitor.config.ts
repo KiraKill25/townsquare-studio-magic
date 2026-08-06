fad63296440bcf202b8c65913ffb2af2602e169c
KiraKill25 <45837225+KiraKill25@users.noreply.github.com>
2026-08-06 21:41:51 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mouradsville.game',
   appName: "Mourad's Ville",
-  webDir: 'dist',
+  webDir: '.output/public',
   server: {
     androidScheme: 'https'
   },
